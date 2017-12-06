e57f81afe373bab92aaf2e66f5449cf22a98e402
Fangliang Xue <faxue@microsoft.com>
2017-12-06 15:50:39 -0800

    add basic nugets

---
+++ AzureSignin/AzureSignin/AccessKeyVault.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Threading.Tasks;
+
+namespace AzureSignin
+{
+    internal static class AccessKeyVault
+    {
+        const string ProddiagVaultBaseUrl = "https://vsproddiag-test.vault.azure.net/";
+        const string ProddiagSecretName = "CertString";
+
+        public static async Task<string> Access()
+        {
+            string authority = String.Format(Configuration.Authority, Configuration.TenantId);
+            AuthenticationContext authContext = new AuthenticationContext(authority);
+            AuthenticationResult result = null;
+
+            try
+            {
+                // See if token is present in cache
+                result = await authContext.AcquireTokenSilentAsync("https://vault.azure.net/", Configuration.ClientId).ConfigureAwait(false);
+
+            }
+            catch (Exception e)
+            {
+                UserCredential userCredential = new UserCredential();
+
+                try
+                {
+                    result = await authContext.AcquireTokenAsync("https://vault.azure.net/", Configuration.ClientId, userCredential).ConfigureAwait(false);
+                }
+                catch (Exception exp)
+                {
+                }
+            }
+
+            return "";
+        }
+    }
+}
