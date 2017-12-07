8a5db62d6c26af006a92879fca49792a7e07ef4e
Fangliang Xue <faxue@microsoft.com>
2017-12-06 16:51:26 -0800

    using old AppAuthentication framework works

---
+++ AppAuthenticationOldNuget/AppAuthenticationOldNuget/AccessWithKeyVaultCertificate.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace E2E
+{
+    public static class AccessWithKeyVaultCertificate
+    {
+        const string ProddiagVaultBaseUrl = "https://vsproddiag-test.vault.azure.net/";
+        const string ProddiagSecretName = "CertString";
+
+        public static async Task<string> Access()
+        {           
+            // According to https://docs.microsoft.com/en-us/azure/key-vault/service-to-service-authentication
+            // For local development, AzureServiceTokenProvider fetches tokens with Azure AD Integrated Authentication. However we can 
+            // only use it to access keyVault, trying  to access management resource will fail because 2FA is required
+            //var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            //string accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("https://vault.azure.net/").ConfigureAwait(false);
+
+            var azureServiceTokenProvider = new AzureServiceTokenProvider("AuthenticateAs=User");
+            string certString = string.Empty;
+            using (var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback)))
+            {
+                SecretBundle secretBundle = await kv.GetSecretAsync(ProddiagVaultBaseUrl, ProddiagSecretName);
+                certString = secretBundle.Value;
+
+                // The following code won't work, once certificate is uploaded into KeyVault, the certificate we get back from 
+                // KeyVault will only contain the public key and AcquireTokenAsync will complain about missing private key
+                /*
+                CertificateBundle certBundle = await kv.GetCertificateAsync(ProddiagVaultBaseUrl, ProddiagCertName);
+                X509Certificate2 cert = new X509Certificate2(certBundle.Cer);
+                string authority = String.Format(Authority, TenantId);
+                AuthenticationContext authContext = new AuthenticationContext(authority);
+                var token = await authContext.AcquireTokenAsync("https://management.azure.com/", new ClientAssertionCertificate(ClientId, cert));
+                */
+            }
+
+            X509Certificate2 cert = new X509Certificate2();
+            cert.Import(Convert.FromBase64String(certString));
+
+            // If you want to serialize the certificate to a file
+            //byte[] bytes = Convert.FromBase64String(certString);
+            //System.IO.File.WriteAllBytes("D:\\cert.pfx", bytes);
+
+            string authority = String.Format(Configuration.Authority, Configuration.TenantId);
+            AuthenticationContext authContext = new AuthenticationContext(authority);
+            AuthenticationResult token = await authContext.AcquireTokenAsync("https://management.azure.com/", new ClientAssertionCertificate(Configuration.ClientId, cert));           
+
+            return token.AccessToken;
+        }
+    }
+}
--- AppAuthenticationOldNuget/AppAuthenticationOldNuget/Program.cs
+++ AppAuthenticationOldNuget/AppAuthenticationOldNuget/Program.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AppAuthenticationOldNuget
+namespace E2E
 {
+    internal static class Configuration
+    {
+        public const string Authority = "https://login.microsoftonline.com/{0}";
+        public const string TenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47";
+        public const string ClientId = "2de6aaf5-2694-4563-a47c-110df3c0ffc6";
+        public const string SubscriptionId = "1129b994-1ec6-487b-a948-7fef2a413d26";
+        public const string CertificateSubjectName = "CN=homecert3";
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var result = AccessWithKeyVaultCertificate.Access().Result;
+
+            ResourceManager.RunSample();
         }
     }
 }
